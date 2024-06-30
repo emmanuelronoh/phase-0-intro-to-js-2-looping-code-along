99b8acd24354eded7cf900c58ab65f8fd7bbb3db
emmanuel ronoh <eronoh036@gmail.com>
2024-06-30 10:25:41 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+//   Function to create thank you messages for an array of names
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
